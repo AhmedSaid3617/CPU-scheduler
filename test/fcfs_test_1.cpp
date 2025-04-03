442cd28338f77c70ee8983a6209d09dd6eff3b1e
Shams M. Monem <shamsmmonem@gmail.com>
2025-04-03 13:52:09 +0200

    Used GoogleTest instead of CTest

--- test/fcfs_test_1.cpp
+++ test/fcfs_test_1.cpp
@@ -1,6 +1,14 @@
 #include <common/Scheduler.h>
 #include <schedulers/FCFS_Scheduler.h>
 
-int main() {
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
     Scheduler * sch = new FCFS_Scheduler();
+
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
 }
