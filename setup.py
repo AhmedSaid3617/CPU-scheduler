d79883f5c513291e2337ea00009073c0f92d1666
kemo <halabykareem@example.com>
2025-04-04 19:30:04 +0200

    configuration file 3

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cpu_scheduler_simulator",
+    version="0.1",
+    description="CPU scheduler simulator project",
+    license="MIT",
+    packages=find_packages(include=["core.common", "core.schedulers"]),
+    python_requires=">=3.6",
+    install_requires=[],
+    extras_require={
+        "test": ["pytest>=7.0", "setuptools"]
+    },
+    test_suite="pytest",
+)
